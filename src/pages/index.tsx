4725eeed5a41c213a188017e82d3a7b9499f5349
sayaanalam <31066332+sayaanalam@users.noreply.github.com>
2024-05-20 17:00:58 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { compareDesc } from 'date-fns';
 import { GetStaticProps } from 'next';
 import { NextSeo } from 'next-seo';
 
@@ -7,19 +6,13 @@ import { PageTitle } from '../components/PageTitle';
 import { Photos } from '../components/Photos';
 import { Resume } from '../components/Resume';
 import { SocialLink } from '../components/SocialLink';
-import { NotePreview } from '../components/notes/NotePreview';
 import { About, Name, SocialMedia } from '../data/lifeApi';
-import { Note, notesApi } from '../lib/notesApi';
 
 const seoTitle = 'Bartosz Jarocki';
 const seoDescription =
   'A passionate software engineer with an eye for details based in Wrocław, Poland.';
 
-type Props = {
-  latestNotes: Note[];
-};
-
-export default function Home({ latestNotes }: Props) {
+export default function Home() {
   return (
     <>
       <NextSeo
@@ -52,28 +45,17 @@ export default function Home({ latestNotes }: Props) {
       </Container>
       <Photos />
       <Container className="mt-12">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col gap-16">
-            {latestNotes.map((blogPost) => (
-              <NotePreview key={blogPost.slug} note={blogPost} dense />
-            ))}
-          </div>
-          <div className="lg:ml-auto space-y-10 lg:pl-16 xl:pl-24">
-            <Resume />
-          </div>
+        <div className="lg:ml-auto space-y-10 lg:pl-16 xl:pl-24">
+          <Resume />
         </div>
       </Container>
     </>
   );
 }
 
-const NEWEST_POSTS_TO_DISPLAY = 5;
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const latestNotes = await notesApi.getNotes('desc', NEWEST_POSTS_TO_DISPLAY);
-
+export const getStaticProps: GetStaticProps = async () => {
   return {
-    props: { latestNotes },
+    props: {},
     revalidate: 10,
   };
 };
